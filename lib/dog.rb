55fe2aaeca20f32f7ae256bb7c4cbe503a778f21
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-07 04:58:54 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+class Dog
+  
+  
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+ 
+  def name, breed=(name, breed)
+    @name = name
+    @breed = breed
+  end
+ 
+  def name, breed
+    @name
+    @breed
+  end
+ 
+ end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
